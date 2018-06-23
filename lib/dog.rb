7d896084852822e12a9979515d6c56f10c846024
Alejandra Rocha <arocha94@gmail.com>
2018-06-23 15:55:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,22 @@
 
 
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
  
   def name
-    @this_dogs_name
+    @name 
   end
+ 
+  def name=(name)
+    @name = name
+  end
+  
+  def breed
+    @breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+  
 end
 
