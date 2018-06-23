38f6ea4423eea17b77d38b926cc45fb7c2920782
Alejandra Rocha <arocha94@gmail.com>
2018-06-23 15:55:58 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 # person.rb
 
+class Person
+ 
+  def name
+    @name 
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    @job
+  end
+ 
+  def job=(job)
+    @job = job
+  end
+  
+end
+
 
